3298f113692aa062d1b3ed05de4fbaddcf042b74
remyJardillier <remy.jardillier@laposte.net>
2017-02-16 00:16:32 +0100

    Update test_search_func.R

--- Information_Extraction/test_search_func.R
+++ Information_Extraction/test_search_func.R
@@ -5,8 +5,7 @@ source(file = "tagPOS.R")
 source(file = "search_date.R")
 source(file = "search_policy_id.R")
 source(file = "search_date_dummy.R")
-
-library(tm)
+source(file = "search_claim.R")
 
 # create the corpus
 setwd(dir = "D:/Documents/Centrale/3A/seminaire_AXA/dossier_robin/FICHES_TRIEES")
@@ -26,54 +25,39 @@ docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, content_transformer(tolower))
 
 # diccionary
-dic <- list(date = list(level1 = c("date"), level2 = c("loss", "incident", "sinister", "event")),
+dic <- list(date = list(level1 = c("date"), 
+                        level2 = c("loss", "incident", "sinister", "event")),
             
-            policy_id = list( level1 = c("policy"), level2 = c("number", "no"), level3 = c("period")),
+            policy_id = list( level1 = c("policy"), 
+                              level2 = c("number", "no", "nâº", "nº"), 
+                              level3 = c("period")),
             
-            cause = c("hurricane", "floods", "fire", "earthquake"),
+            claim = list(level1 = list("claim",c("adjusted", "loss"), c("claimed", "amount"), c("loss", "estimate"))),
             
-            localisation = c("location of event", "place of loss", "address", "insured company", "insured"),
-            
-            couts_sinistres = c(" Claims Reserve Estimation", "claim"),
-            
-            valeurs_biens_assures = c("total", "sub total"))
-
-# function to extract the information
-IE <- function(doc, dic){
-  #--------------------------------------------------#
-  
-  # tag le doc avec la fonction tagPOS défini précédemment
-  doc_tagged <- tagPOS(doc) 
-  
-  #----------------------------------------------------#
-  # Information extraction # 
-  
-  # date: 
-  date <- search_date(doc_tagged, dic)
-  
-  # policy_id:
-  policy_id <- search_policy_id(doc_tagged, dic)
-  
-  return(list(date = date, policy_id = policy_id))
-}
+            adress = list()
+            )
 
 # table to save the results
-table_test <- data.frame(matrix(ncol = 3, nrow = length(docs)))
-colnames(table_test) = c("name", "date", "policy_id")
+table_test <- data.frame(matrix(ncol = 4, nrow = length(docs)))
+colnames(table_test) = c("name", "date", "policy_id", "claim")
 table_test[,1] <- names(docs)
 
 # we apply the function IE on each text of the corpus
-for(k in 22:length(docs)){
-  infos <- IE(docs[[k]], dic) 
+for(k in 1:length(docs)){
   
-  table_test[k,2] <- infos$date
-  table_test[k,3] <- infos$policy_id
+  # tag le doc avec la fonction tagPOS défini précédemment
+  doc_tagged <- tagPOS(docs[[k]]) 
+  
+  
+  #table_test[k,2] <- search_date(doc_tagged, dic)
+  #table_test[k,3] <- search_policy_id(doc_tagged, dic)
+  table_test[k,4] <- search_claim(doc_tagged, dic)
 }
 
 # k = 2: pb avec la référence: tagged as "DT"
 # k = 4: pas de date dans la fiche
 
-#↨-------------------------------------------------------------------#
+#???-------------------------------------------------------------------#
 # comparaison des deux fonctions "search_date", "search_date_dummy":
 
 # table to save the results
